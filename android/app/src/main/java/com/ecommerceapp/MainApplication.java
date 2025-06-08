60c0d62d47a9cb3561f2e7eed771d9f12e6fe83f
Rdj23 <rohan.jadhav@clevertap.com>
2025-06-09 01:22:54 +0530

    new arch false

--- android/app/src/main/java/com/ecommerceapp/MainApplication.java
+++ android/app/src/main/java/com/ecommerceapp/MainApplication.java
@@ -34,8 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
 
          @Override
          public boolean isNewArchEnabled() {
-            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
+            return false;
+         }
 
         @Override
         public Boolean isHermesEnabled() {
@@ -58,9 +58,6 @@ public class MainApplication extends Application implements ReactApplication {
         // Hook up CleverTap for React Native:
         CleverTapRnAPI.initReactNativeIntegration(this);
 
-        // Load New Architecture if you opted in:
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            DefaultNewArchitectureEntryPoint.load();
-        }
+       
     }
 }
