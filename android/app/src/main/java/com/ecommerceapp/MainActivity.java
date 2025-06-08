5ccb741a4d6db07dfe6c4d33edafd7ec2da46555
Rdj23 <rohan.jadhav@clevertap.com>
2025-06-09 00:40:14 +0530

    converted to java, and added google service json file, fix the issue for sdk location by creating local.propeties file and fix the build erros for setup

---
+++ android/app/src/main/java/com/ecommerceapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ecommerceapp;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.clevertap.react.CleverTapRnAPI;  // for deep-link integration
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Tell CleverTap about any launch deep-link:
+        CleverTapRnAPI.setInitialUri(getIntent().getData());
+    }
+
+    @Override
+    protected String getMainComponentName() {
+        return "ecommerceApp";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        // If you’re not yet using Fabric/JSI you can shorten this to
+        //   return new ReactActivityDelegate(this, getMainComponentName());
+        return new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
+            DefaultNewArchitectureEntryPoint.getFabricEnabled()  // or .isFabricEnabled()
+        );
+    }
+}
