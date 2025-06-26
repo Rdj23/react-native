17344bbf25916cc5f579bc3f0214d79f01139c69
Rdj23 <rohan.jadhav@clevertap.com>
2025-06-26 22:22:51 +0530

    fix for android crash

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// index.js
-
 import 'react-native-gesture-handler';
 
 
-// ✅ Must go at the top BEFORE any RN module is touched
-if (typeof global.ErrorUtils === 'undefined') {
-  global.ErrorUtils = {
-    setGlobalHandler: () => {},
-    reportFatalError: (e) => console.error('[SafeFallbackError]', e),
-  };
-} else {
-  if (typeof global.ErrorUtils.setGlobalHandler !== 'function') {
-    global.ErrorUtils.setGlobalHandler = () => {};
-  }
-}
 
-
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
